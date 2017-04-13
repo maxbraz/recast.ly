2b91d258a696da40346c118ee8fe30528da67543
Hack Reactor Student <student@hackreactor.com>
2017-04-13 11:47:52 -0700

    implement choose video in videoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: 0
+      currentVideo: window.exampleVideoData[0]
     };
     this.onVideoClick = this.onVideoClick.bind(this);
   }
 
   onVideoClick(event) {
-    // console.log('I got clicked');
-    // console.log('onVideoClick event: ', event);
-    // let context = this;
-    // console.log('onVideoClick event this is: ', event.target);
-    let videoID = event.target.getAttribute('id');
-    console.log(videoID);
+    let searchID = event.target.getAttribute('id');
+    let filteredVideo = window.exampleVideoData.filter(vid => vid.id.videoId === searchID )[0];
+
     this.setState({
-      clicked: videoID
+      currentVideo: filteredVideo
     });
   }
 
   render () {
+    let selectedVideo = this.state.currentVideo;
+
     return (
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer video={this.state.clicked || window.exampleVideoData[0]} />
+          <VideoPlayer video={selectedVideo} />
         </div>
         <div className="col-md-5">
           <VideoList videos={window.exampleVideoData} clickFunction={this.onVideoClick}/>
