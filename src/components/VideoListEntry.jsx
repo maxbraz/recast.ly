dc2751024da4b672d7396f8555d8bfccc22ebecd
Hack Reactor Student <student@hackreactor.com>
2017-04-12 17:38:59 -0700

    imported mock video data

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,24 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry">
+        <div className="media-left media-middle">
+          //this.snippet.thumbnails.default.url
+          <img className="media-object" src="this.props.snippet.thumbnails.default.url" alt="" />
+        </div>
+        <div className="media-body">
+          //this.snippet.title
+          <div className="video-list-entry-title">{this.props.snippet.title}</div>
+          //this.snippet.description
+          <div className="video-list-entry-detail">Video Description</div>
+        </div>
+      </div>
+  );
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
