e9e6d5119864a4625e966d7d94a3a36ad49ca549
Hack Reactor Student <student@hackreactor.com>
2017-04-13 12:32:43 -0700

    add youtubeAPI creds

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // $.get({'https://www.googleapis.com/youtube/v3/search'
+  //   data: {key: options.key, query: options.query, max: options.max},
+  //   key: options.key,
+  //   query: options.query,
+  //   max: options.max,
+  //   success: function(data) {
+  //     cb(data);
+  //     console.log('this is the get data', data);
+  //   },
+  //   error: function(data) {
+  //     console.log('error: failed to retrieve from youtube: ', data)
+  //   }
+  // });
 };
 
 window.searchYouTube = searchYouTube;
