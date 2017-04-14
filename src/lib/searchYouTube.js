324664fa1da2318290907f12eb5fc84e939e5bc0
Hack Reactor Student <student@hackreactor.com>
2017-04-13 17:54:41 -0700

    Add searchYoutube functionality

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,17 +1,20 @@
-var searchYouTube = (options, callback) => {
-  // $.get({'https://www.googleapis.com/youtube/v3/search'
-  //   data: {key: options.key, query: options.query, max: options.max},
-  //   key: options.key,
-  //   query: options.query,
-  //   max: options.max,
-  //   success: function(data) {
-  //     cb(data);
-  //     console.log('this is the get data', data);
-  //   },
-  //   error: function(data) {
-  //     console.log('error: failed to retrieve from youtube: ', data)
-  //   }
-  // });
+var searchYouTube = ({query, max, key}, cb) => {
+  let baseURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=';
+  let searchPartURL = `${query}&maxResults=${max}&key=${key}&type=video&videoEmbeddable=true`;
+  let youTubeSearchURL = baseURL + searchPartURL;
+
+  $.ajax({
+    url: youTubeSearchURL,
+    type: 'GET',
+    part: 'snippet',
+    contentType: 'application/json; charset=utf-8'
+  })
+  .done(function(data) {
+    cb(data.items);
+  })
+  .fail(function(data) {
+    console.log('error: ', data);
+  });
 };
 
 window.searchYouTube = searchYouTube;
